6ab52096c0a6193b9609c8065db35b5c80a1c47f
Felix Karg <f.karg10@gmail.com>
2021-09-29 14:20:04 +0200

    update shell.nix

--- node-ts/shell.nix
+++ node-ts/shell.nix
@@ -3,13 +3,15 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "node";
     buildInputs = [
+        fish
         nodejs-12_x
         yarn
         nodePackages.typescript
+        mongodb-4_2
     ];
     shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
-        fish --init-command="function fish_greeting; echo 'Entered NodeJS Environment'; end; function fish_prompt; echo 'NodeJS 🐟> '; end; npm run build"
+        fish --init-command="function fish_greeting; echo 'Entered NodeJS Environment'; end; function fish_prompt; echo -n 'NodeJS '; set_color green; echo -n (prompt_pwd); set_color normal ; echo -n ' 🐟> '; end"
         echo "Leaving NodeJS Environment"
         exit
     '';
