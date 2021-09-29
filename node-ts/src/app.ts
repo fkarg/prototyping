320a9f1451bd8e81c382c1c5ac3220d114db4fd6
Felix Karg <f.karg10@gmail.com>
2021-09-29 14:19:55 +0200

    continue following tutorial

--- node-ts/src/app.ts
+++ node-ts/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 
 import log from "./logger";
+import connect from "./db/connect";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
@@ -13,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
-  console.log(log);
-  console.log(`Server listing at http://${host}:${port}`);
+  log.info(`Server listing at http://${host}:${port}`);
+
+  connect()
 });
 
